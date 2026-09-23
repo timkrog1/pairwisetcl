9656a2b96bf9e6f51e5daebf7e17f4fb6a7054df
timkrog1 <107715312+timkrog1@users.noreply.github.com>
2026-09-23 15:47:32 -0500

    Update setup.py
    Changed name of package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='pytcl',
+    name='pairwisetcl',
     version='1.0.0',
     description='A Python package for calculating electron spin dephasing due to pairwise nuclear spin flip-flops using the Time-convolutionless master equation',
     url='https://github.com/timkrog1/pytcl',
     author='Timothy J. Krogmeier',
     author_email='krogm033@umn.edu',
-    packages=['pytcl'],
+    packages=['pairwisetcl'],
     install_requires=[
         'numpy>=1.24.0,<2.0.0',
         'pyscf>=2.0.0'
